5fe50f7f25dbb31082d7e494626927e28d297c4d
Gyana Ranjan Panda <grp.gyanaranjan@gmail.com>
2023-04-17 05:39:47 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
+import { base_url } from "./utils/apiBaseUrl";
+import { Link, useNavigate } from "react-router-dom";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await axios.post(`${base_url}/create`, {
+      email
+    }
+    )
+    localStorage.setItem('email', email)
+    if (res.data.status === 'success') {
+      navigate('/quiz')
+    }
+    console.log(email);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-lg  min-h-screen bg-[#345778] p-4 flex justify-center items-center flex-col relative">
+      <Link to='admin-login'>
+        <div className="absolute top-10 text-white right-10 border-2 border-white rounded p-1 cursor-pointer">Admin Pannel</div>
+      </Link>
+      <h1 className="text-white text-3xl text-center">Treasure Hunt</h1>
+      <div className="flex justify-center ">
+        <div className="bg-white w-fit p-10 rounded-lg">
+          <h2 className="text-2xl text-center">Welcome to the Treasure Hunt</h2>
+          <p className="text-center">Please enter your email to start the game</p>
+          <div className="flex justify-center">
+            <form onSubmit={handleSubmit}>
+              <input
+                type="email"
+                placeholder="Enter your email"
+                required
+                autoFocus
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="border-2 border-gray-300 p-2 rounded-lg mt-3"
+              />
+
+              <button className="bg-blue-500 text-white p-2 rounded-lg px-4 ml-3">
+                Start
+              </button>
+            </form>
+          </div>
+          <Link to='/leaderboard'>
+            <p className="mt-3 text-blue-500 cursor-pointer">See LeaderBoard instead?</p>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Quiz from './pages/quiz';
+import LeaderBoard from './pages/leaderboard';
+import AdminLogin from './pages/adminSigin';
+import Home from './pages/Home';
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/quiz",
+    element: <Quiz />,
+  },
+  {
+    path: "*",
+    element: <div>Not Found</div>,
+  },
+  {
+    path: "/leaderboard",
+    element: <LeaderBoard />,
+  }, {
+    path: "/admin-login",
+    element: <AdminLogin />,
+  }, {
+    path: "/home",
+    element: <Home/>,
+  }
+]);
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
